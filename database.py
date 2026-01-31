4c24e51cb54cc51f4f637f12ab351d84463b6fd7
Rogoberto Ribeiro <usbetin@gmail.com>
2026-01-31 01:17:13 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,24 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL não encontrada")
-
-# 🔥 força psycopg v3
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace(
-        "postgres://",
-        "postgresql+psycopg://",
-        1
-    )
-elif DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace(
-        "postgresql://",
-        "postgresql+psycopg://",
-        1
-    )
 
 engine = create_engine(
     DATABASE_URL,
@@ -34,12 +20,6 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 
