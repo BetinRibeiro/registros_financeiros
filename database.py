d4bddf0e960801ff03608f766bc170f203fe8d90
Rogoberto Ribeiro <usbetin@gmail.com>
2026-01-31 00:11:21 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+#configurado no servidor
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
